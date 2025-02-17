4e4f57b7f6508c44ad4f45a86ce0a6581507977b
Samleoty <156793938+Samleoty@users.noreply.github.com>
2025-02-17 15:42:25 +0000

    Add files via upload

---
+++ 5218/database_connection.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost"; 
+$username = "root"; 
+$password = "root"; 
+$dbname = "SHOPPINGWEB"; 
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected successfully to MySQL database!";
+}
+?>
